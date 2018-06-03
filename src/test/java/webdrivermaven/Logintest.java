f79f0729e83442ae40d400428d9f8cc2f6b80b8b
PelluriSurendra <pellurisure@gmail.com>
2018-06-04 00:14:52 +0530

    intial commit

---
+++ src/test/java/webdrivermaven/Logintest.java
@@ -0,0 +1,30 @@
+package webdrivermaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Logintest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setup()
+	{
+		driver =new ChromeDriver();
+	}
+	@Test
+	public void doLogin()
+	{
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("abc@yopmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("ssssddd");
+	}
+    @AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
